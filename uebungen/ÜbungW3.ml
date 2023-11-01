12e3644d12ddd1d13f8201b87495dac4c424c944
monnerjahnl0 <s6lumonn@uni-bonn.de>
2023-11-01 19:23:42 +0100

    First solution to exercice 3

--- uebungen/ÜbungW3.ml
+++ uebungen/ÜbungW3.ml
@@ -1,5 +1,5 @@
 (* Hier werden die Dateien von der Website von Harrison benötigt, alternativ, kann man vorher im Terminal einmal "init.ml" laden*)
-#use "init.ml";;          
+#use "src/init.ml";;          
 (*
 1. Schreiben sie eine Funktion, ähnlich zu onatoms, die als Input eine Formel fm hat, in der substituiert werden soll,
  eine Formel x, die substituiert werden soll und eine Formel y, die eingesetzt wird. Sie soll also einer Substitution einer Teilformel entsprechen
@@ -7,6 +7,17 @@
 
 
 let rec substitute fm x y = 
+  match fm with
+  True -> True
+  | False -> False
+  | Atom(a) -> if (Atom(a) = x) then y else Atom(a)
+  | Not(a) -> Not(substitute a x y)
+  | And(a,b) -> And(substitute a x y, substitute b x y)
+  | Or(a,b) -> Or(substitute a x y, substitute b x y)
+  | Imp(a,b) -> Imp(substitute a x y, substitute b x y)
+  | Iff(a,b) -> Iff(substitute a x y, substitute b x y)
+  | Forall(a,b) -> Forall(a, substitute b x y)
+  | Exists(a,b) -> Exists(a, substitute b x y);;
 
 
 
@@ -30,6 +41,9 @@ let rec atomizer k =
 
 
 let rec makeProp n =
+  match n with
+  0 -> []
+  | n -> (makeProp (n-1)) @ (List.map (fun x -> (And(x, (List.nth (atomizer n) 0)))) (atomizer (n-1))) @ (List.map (fun x -> (Or(x, (List.nth (atomizer n) 0)))) (atomizer (n-1))) @ (List.map (fun x -> (Imp(x, (List.nth (atomizer n) 0)))) (atomizer (n-1))) @ (List.map (fun x -> (Iff(x, (List.nth (atomizer n) 0)))) (atomizer (n-1)));;
 
 
 (*Testfall*)
@@ -41,6 +55,9 @@ let props = makeProp 3 (atomizer 3);;
 
 
 let rec countTaut lfm taut total =
+  match lfm with
+  [] -> (taut, total)
+  | fm::rest -> if (tautology fm) then (countTaut rest (taut+1) (total+1)) else (countTaut rest taut (total+1));;
 
 (*Testfall*)
 countTaut props 0 0;;
