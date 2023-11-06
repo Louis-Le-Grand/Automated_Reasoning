fe04f19d8899852ff18712af1310e8dcf9937d61
monnerjahnl0 <s6lumonn@uni-bonn.de>
2023-11-07 00:23:57 +0100

    Bux fix and Test

--- uebg/W4.ml
+++ uebg/W4.ml
@@ -1,3 +1,6 @@
+#use "./src/init.ml";;
+
+
 (*
     Write a new tautology Function which checks if
     a formula is valid or not by using the CNF.
@@ -11,7 +14,7 @@
 let rec sear_compl l storage = 
     match l with
     | [] -> false
-    | hd::tl -> if List.mem (neg hd) storage then true else sear_compl tl storage;;
+    | hd::tl -> if List.mem (Not(hd)) storage then true else sear_compl tl storage;;
 
 (*
     it_conj will at the end iterate over the CNF and
@@ -21,7 +24,7 @@ let rec sear_compl l storage =
 let rec it_conj cnf_set = 
     match cnf_set with
     | [] -> true
-    | hd::tl -> if sear_compl hd hd then false else it_conj tl;;
+    | hd::tl -> if sear_compl hd hd then false else it_conj tl;; (* TODO: is that so ??? if sear_compl returns true, then the conjunct is not valid *)
 
 
 (*
@@ -30,3 +33,14 @@ let rec it_conj cnf_set =
 *)
 
 let tautology fm = it_conj (purecnf fm);;
+
+(* Test *)
+let x = And (Or (Var "a", Var "b"), Or (Not (Var "a"), Var "c"));;
+let y = And (Or (Var "a", Var "b"), Or (Not (Var "a"), Not (Var "b")));;
+let z = And (Or (Var "a", Not (Var "b")), Or (Not (Var "a"), Var "b"));;
+let w = And (Or (Var "a", Not (Var "a")), Or (Not (Var "a"), Var "a"));;
+
+tautology x;;
+tautology y;;
+tautology z;;
+tautology w;;
