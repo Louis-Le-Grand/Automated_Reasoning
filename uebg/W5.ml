8b1fdccb40934e01dba09e082c9f9cb0087e4531
monnerjahnl0 <s6lumonn@uni-bonn.de>
2023-11-16 01:07:56 +0100

    W5 V2

--- uebg/W5.ml
+++ uebg/W5.ml
@@ -12,14 +12,15 @@ let default_parser = parse_prop_formula;;
 
 let one_literal_rule_assignment clauses assignment =
   let u = hd (find (fun cl -> length cl = 1) clauses) in
-  let assignment = (u, true) @ assignment in
+  let new_assignment = [(u, true)] @ assignment in
   let u' = negate u in
   let clauses1 = filter (fun cl -> not (mem u cl)) clauses in
-  image (fun cl -> subtract cl [u']) clauses1;;
+  let new_clauses = image (fun cl -> subtract cl [u']) clauses1 in
+  (new_clauses, new_assignment);;
 
 let affirmative_negative_rule_assignment clauses assignment =
   let neg',pos = partition negative (unions clauses) in
-  let assignment = map (fun l -> (l, true)) pos @ map (fun l -> (l, false)) neg' @ assignment in
+  let new_assignment = map (fun l -> (l, true)) pos @ map (fun l -> (l, false)) neg' @ assignment in
   let neg = image negate neg' in
   let pos_only = subtract pos neg and neg_only = subtract neg pos in
   let pure = union pos_only (image negate neg_only) in
@@ -38,7 +39,7 @@ let rec dpll_assignment clauses assignment =
   try dpll_assignment(affirmative_negative_rule_assignment clauses assignment) with Failure _ ->
   let pvs = filter positive (unions clauses) in
   let p = maximize (posneg_count clauses) pvs in
-  dpll_assignment (insert [p] clauses) assignment or dpll_assignment (insert [negate p] clauses) assignment;;
+  dpll_assignment (insert [p] clauses) new_assignment or dpll_assignment (insert [negate p] clauses) new_assignment;;
 
 
 let ex1 = defcnfs <<(a \/ b) /\ (a \/ ~c \/ d) /\ (b \/ c \/ ~d) /\ (~a) /\ (a \/ ~d)>>;;
@@ -48,4 +49,4 @@ let ex2 = defcnfs <<(a \/ b) /\ (a \/ ~b \/ c) /\ (b \/ ~c) /\ (b \/ c)>>;;
 dpll_assignment ex2 [];;
 
 let ex3 = defcnfs << (p \/ q \/ r) /\ (~r \/ ~q \/ p) /\ (~p \/ ~q) /\ (q \/ ~p)>>;;
-dppl_assignment ex3 [];;
+dpll_assignment ex3 [];;
